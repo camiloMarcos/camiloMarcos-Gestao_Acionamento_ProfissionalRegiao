c760df205f6edddf7c941e1894059d0a19031af7
CamiloMarcos <marcoshcamilo@gmail.com>
2026-02-28 23:51:43 -0300

    commit muito tempo s codar

--- src/main/java/br/com/univida_test/demo/exceptions/GlobalException.java
+++ src/main/java/br/com/univida_test/demo/exceptions/GlobalException.java
@@ -12,56 +12,55 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import jakarta.servlet.http.HttpServletRequest;
 
-
 //É uma anotação usada para tratar exceções e aplicar lógica comum a todos os controllers de um aplicativo Spring.
 @ControllerAdvice
 public class GlobalException {
 
-    private LocalDateTime timestamp;
-    private Integer status;
-    private String error;
-    private String message;
-    private String path;
-
-
-
-//É uma anotação do Spring Framework usada para tratar exceções de forma centralizada dentro de um Controller ou RestController.
+    // É uma anotação do Spring Framework usada para tratar exceções de forma
+    // centralizada dentro de um Controller ou RestController.
     @ExceptionHandler(ObjectNotFoundException.class)
-    public ResponseEntity<StandardError> objectNotFoundException (ObjectNotFoundException e, HttpServletRequest request){
-        StandardError se = new StandardError(LocalDateTime.now(),HttpStatus.NOT_FOUND.value(), e.getMessage(), request.getRequestURI());
+    public ResponseEntity<StandardError> objectNotFoundException(ObjectNotFoundException e,
+            HttpServletRequest request) {
+        StandardError se = new StandardError(LocalDateTime.now(), HttpStatus.NOT_FOUND.value(), e.getMessage(),
+                request.getRequestURI());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(se);
     }
 
-
-//IllegalArgumentException em Java é uma exceção que acontece quando você passa um argumento inválido ou inadequados para um método.
-//erro não está na lógica interna do método, mas sim nos valores passados pelo usuário ou pelo código chamador.
+    // IllegalArgumentException em Java é uma exceção que acontece quando você passa
+    // um argumento inválido ou inadequados para um método.
+    // erro não está na lógica interna do método, mas sim nos valores passados pelo
+    // usuário ou pelo código chamador.
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<StandardError> illegalArgumentException (IllegalArgumentException e, HttpServletRequest request){
-        StandardError se = new StandardError(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(), e.getMessage(), request.getRequestURI());
+    public ResponseEntity<StandardError> illegalArgumentException(IllegalArgumentException e,
+            HttpServletRequest request) {
+        StandardError se = new StandardError(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(), e.getMessage(),
+                request.getRequestURI());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(se);
     }
 
-
-//quando ocorre uma violação de integridade de dados em uma op no DB.Chave pri duplicada, Campos obrigatórios (NOT NULL) não preenchidos
+    // ExceptionHandler, quando ocorre uma violação de integridade de dados em uma
+    // op no DB. Chave pri duplicada, Campos obrigatórios (NOT NULL) não preenchidos
     @ExceptionHandler(DataIntegrityViolationException.class)
-    public ResponseEntity<StandardError> dataIntegrityViolationException (DataIntegrityViolationException e, HttpServletRequest request){
-        StandardError se = new StandardError (LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(), e.getMessage(), request.getRequestURI());
+    public ResponseEntity<StandardError> dataIntegrityViolationException(DataIntegrityViolationException e,
+            HttpServletRequest request) {
+        StandardError se = new StandardError(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(), e.getMessage(),
+                request.getRequestURI());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(se);
     }
 
-    //FAZER O TRATAMENTO PARA MethodArgumentNotValidException -> AULA DE VALIDAÇÃO
+    // FAZER O TRATAMENTO PARA MethodArgumentNotValidException -> AULA DE VALIDAÇÃO
 
-       @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<StandardError> methodArgumentNotValidException (MethodArgumentNotValidException e, HttpServletRequest request){
-        ValidationError error = new ValidationError (LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(), "Erro de Validação dos Campos", request.getRequestURI());
-        for(FieldError obj: e.getBindingResult().getFieldErrors()){
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> methodArgumentNotValidException(MethodArgumentNotValidException e,
+            HttpServletRequest request) {
+        ValidationError error = new ValidationError(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(),
+                "Erro de Validação dos Campos", request.getRequestURI());
+        for (FieldError obj : e.getBindingResult().getFieldErrors()) {
             error.addError(obj.getField(), obj.getDefaultMessage());
         }
-        
+
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 
     }
-
-
 
 }
--- src/main/java/br/com/univida_test/demo/service/ProfissionalService.java
+++ src/main/java/br/com/univida_test/demo/service/ProfissionalService.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import br.com.univida_test.demo.models.Profissional;
 import br.com.univida_test.demo.repositories.ProfissionalRepository;
@@ -16,41 +15,45 @@ public class ProfissionalService {
     @Autowired
     private ProfissionalRepository profissionalRepository;
 
-
     // Listar todos os profissionais -> se a lista estiver vazia.
     public List<Profissional> findAll() {
         List<Profissional> listProf = profissionalRepository.findAll();
         if (listProf.isEmpty()) {
-            throw new ObjectNotFoundException("Nenhum profissional cadastrado");      }
+            throw new ObjectNotFoundException("Nenhum profissional cadastrado");
+        }
         return listProf;
     }
 
-
-    // Buscar profissional por ID -> se o ID não existir, ele retorna um ObjectNotFoundException.
+    // Buscar profissional por ID -> se o ID não existir, ele retorna um
+    // ObjectNotFoundException.
     public Profissional findById(Integer id) {
         if (id == null || id <= 0) {
             throw new IllegalArgumentException("ID do profissional não pode ser nulo (DEVE SER MAIOR QUE zero)");
         }
-        return profissionalRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Profissional não encontrado com id " + id));
+        return profissionalRepository.findById(id)
+                .orElseThrow(() -> new ObjectNotFoundException("Profissional não encontrado com id " + id));
     }
 
-
-    /*public Profissional findById(Integer id) {
-        if (id == null || id <= 0) {
-            throw new IllegalArgumentException("ID do profissional não pode ser nulo (DEVE SER MAIOR QUE zero)");
-        }
-        Optional<Profissional> profissional = profissionalRepository.findById(id);
-        if (!profissional.isPresent()) {
-            throw new ObjectNotFoundException(HttpStatus.NOT_FOUND, "Profissional não encontrado com id " + id);      
-        }
-        return profissional.get();
-    }*/
+    /*
+     * public Profissional findById(Integer id) {
+     * if (id == null || id <= 0) {
+     * throw new
+     * IllegalArgumentException("ID do profissional não pode ser nulo (DEVE SER MAIOR QUE zero)"
+     * );
+     * }
+     * Optional<Profissional> profissional = profissionalRepository.findById(id);
+     * if (!profissional.isPresent()) {
+     * throw new ObjectNotFoundException(HttpStatus.NOT_FOUND,
+     * "Profissional não encontrado com id " + id);
+     * }
+     * return profissional.get();
+     * }
+     */
 
     public Profissional save(Profissional profissional) {
         buscarPorNome(profissional);
-               return profissionalRepository.save(profissional);
+        return profissionalRepository.save(profissional);
     }
-
 
     public Profissional update(Profissional profissional) {
         findById(profissional.getId());
@@ -58,36 +61,28 @@ public class ProfissionalService {
         return profissionalRepository.save(profissional);
     }
 
-    //   Deletar profissional por ID -> se o ID não existir, ele retorna um ObjectNotFoundException.
+    // Deletar profissional por ID -> se o ID não existir, ele retorna um
+    // ObjectNotFoundException.
     // ele não verifica se o profissional tem associações, apenas deleta.
     public void delete(Integer id) {
-       Profissional profDelete = findById(id);
-       if (profDelete == null) {
-           throw new ObjectNotFoundException(HttpStatus.NOT_FOUND, "Profissional não encontrado com id " + id);
-       }
-            profissionalRepository.deleteById(id);
+        Profissional profDelete = findById(id);
+        if (profDelete == null) {
+            throw new ObjectNotFoundException("Profissional não encontrado com id " + id);
+        }
+        profissionalRepository.deleteById(id);
     }
-
 
     public void buscarPorNome(Profissional profissional) {
         Optional<Profissional> profPorNome = profissionalRepository.findByNomeIgnoreCase(profissional.getNome());
 
-        if (profPorNome.isPresent()){
-            if (!Objects.equals(profPorNome.get().getId(), profissional.getId())){
+        if (profPorNome.isPresent()) {
+            if (!Objects.equals(profPorNome.get().getId(), profissional.getId())) {
 
                 throw new IllegalArgumentException("Profissional já existente com o nome: " + profissional.getNome());
 
             }
-                
+
         }
-        
-        }
+
     }
-
-    //if (cat.isPresent()) {
-    //  if (!Objects.equals(cat.get().getId(), categoria.getId())) {
-    //      throw new IllegalArgumentException("Categoria já existente com o nome: " + categoria.getNome());
-    //  }
-
-
-
+}
